5423569707602650479d7c58c19fe1d0abaffb2d
Ghaith Shammout <ghaith.shammout@outlook.com>
2026-05-16 09:57:36 +0200

    Added DbContext, DB migrations and README

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
