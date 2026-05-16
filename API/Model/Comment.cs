2b2a809b55ddb4438d747e796a675bc0ab61ad3e
Ghaith Shammout <ghaith.shammout@outlook.com>
2026-05-16 08:25:05 +0200

    Added Comment Model

---
+++ API/Model/Comment.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Model
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        // Navigation property to the Stock entity
+        public Stock? Stock { get; set; }
+        
+    }
+}
